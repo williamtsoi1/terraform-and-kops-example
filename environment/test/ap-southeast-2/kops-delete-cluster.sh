8b54b939d1bf51525df1044a2473c4e96176ccb2
William Tsoi <william@williamtsoi.net>
2018-01-19 10:13:54 +1100

    removing references to snap

--- environment/test/ap-southeast-2/kops-delete-cluster.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-kops delete cluster \
-  --state s3://$(terraform output -module=strata_snap k8s_state_store_bucket_name) \
-  --name kubernetes.$(terraform output -module=strata_snap snap_full_fqdn) \
-  --yes
--- environment/test/ap-southeast-2/main.tf
+++ environment/test/ap-southeast-2/main.tf
@@ -1,36 +1,28 @@
 variable "environment_name" { }
-variable "snap_domain_name" { }
-variable "snap_full_fqdn" { }
+variable "app_domain_name" { }
+variable "app_full_fqdn" { }
 variable "r53_zone_id" { }
 variable "region" { }
 variable "vpc_cidr" { }
 variable "availability_zones" { type = "list" }
 variable "public_subnets" { type = "list" }
 variable "private_subnets" { type = "list" }
-variable "number_of_cassandra_seeds" { }
-variable "cassandra_instance_type" { }
-variable "cassandra_seed_ips" { type = "list" }
-variable "bastion_bucket_name" { }
 
 provider aws {
   region  = "${var.region}"
   profile = "${var.environment_name}"
 }
 
-module "strata_snap" {
+module "my_app" {
   source = "../../../"
   environment_name = "${var.environment_name}"
-  snap_domain_name = "${var.snap_domain_name}"
-  snap_full_fqdn = "${var.snap_full_fqdn}"
+  app_domain_name = "${var.app_domain_name}"
+  app_full_fqdn = "${var.app_full_fqdn}"
   r53_zone_id = "${var.r53_zone_id}"
   vpc_cidr = "${var.vpc_cidr}"
   availability_zones = "${var.availability_zones}"
   public_subnets = "${var.public_subnets}"
   private_subnets = "${var.private_subnets}"
-  number_of_cassandra_seeds = "${var.number_of_cassandra_seeds}"
-  cassandra_instance_type = "${var.cassandra_instance_type}"
-  cassandra_seed_ips = "${var.cassandra_seed_ips}"
-  bastion_bucket_name = "${var.bastion_bucket_name}"
 }
 
 terraform {
